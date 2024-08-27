48397114d348b30730ad0c8f851723141175edf6
Sruti Swaminathan <165972788+sgswamin@users.noreply.github.com>
2024-08-26 22:15:07 -0700

    Initial commit

---
+++ soccer-team/README.md
@@ -0,0 +1,7 @@
+# Soccer Team
+
+* A simple JS project that displays all the soccer team members using arrays and functions.
+
+## Special Thanks
+
+* mimo.org for providing this neat exercise.
---
+++ soccer-team/script.js
@@ -0,0 +1,9 @@
+const goalie = ["Sam"];
+console.log(`Goalie: ${goalie}`);
+const defenders = [];
+defenders.push("Lisa", "Tom", "Jenny", "Jo");
+console.log(`Defenders: ${defenders}`);
+const midfielders = ["Jim", "Kai", "Ian", "Ava"];
+console.log(`Midfielders: ${midfielders}`);
+const forwards = ["Kim", "Lucas"];
+console.log(`Forwards: ${forwards}`);
